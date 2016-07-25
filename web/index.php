4dd6b29b659e841a158d19d214f43c6787f57f42
Rapi Castillo <rapi.castillo@gmail.com>
2016-07-24 23:55:08 -0400

    Changes

--- web/index.php
+++ web/index.php
@@ -91,7 +91,7 @@
                               <a href="http://goo.gl/forms/8SJRDlo7Lx2rUsan1" class="btn btn-default btn-lg"><img src='./img/slack.png' height="20" width="20"> <span class="network-name">Join us in Slack</span></a>
                           </li>
                           <li>
-                              <a href="https://actionnetwork.org/events/progcode-discussion-data" class="btn btn-default btn-lg"><i class="fa fa-calendar-o" aria-hidden="true"></i> <span class="network-name">ProgCode Discussion #001 - Sustainable Data</span></a>
+                              <a href="https://actionnetwork.org/events/progcode-discussion-data" class="btn btn-default btn-lg"><i class="fa fa-calendar-o" aria-hidden="true"></i> <span class="network-name">Discussion #1 - Sustainable Progressive Data</span></a>
                           </li>
                         </ul>
                     </div>

2e2f1af3979d9903e99725b49782810d02f4d2b3
Jordana <jordana@tiac.net>
2018-03-22 12:20:03 -0400

    Update Calendly link.  (https://github.com/ProgressiveCoders/progcode-website/issues/5)

--- web/index.php
+++ web/index.php
@@ -222,7 +222,7 @@
             </div>
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <iframe class="airtable-embed" src="https://airtable.com/embed/shrFyckaO66toNH3U?backgroundColor=cyan&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
+                    <iframe class="airtable-embed" src="https://airtable.com/embed/shrIBemEGOX7y0V5p?backgroundColor=cyan" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
                 </div>
             </div>
           </div>

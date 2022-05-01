350958157ef157cc628239c8a8d16d765e133e6a
Noah Williams <noah@progcode.org>
2022-05-01 10:00:23 -0700

    Add configuration for a droplet with a mounted volume

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -6,3 +6,23 @@ terraform {
     }
   }
 }
+
+resource "digitalocean_droplet" "wordpress" {
+  image  = "ubuntu-22-04-x64"
+  name   = "progcode-wordpress"
+  region = "nyc3"
+  size   = "s-1vcpu-2gb"
+}
+
+resource "digitalocean_volume" "wp-data" {
+  region = "nyc3"
+  name = "wp-data"
+  size = "5"
+  initial_filesystem_type = "ext4"
+  description = "Wordpress content library and /var/lib/mysql"
+}
+
+resource "digitalocean_volume_attachment" "foobar" {
+  droplet_id = digitalocean_droplet.wordpress.id
+  volume_id  = digitalocean_volume.wp-data.id
+}
